f2b1054314653287f57e9e9e3539192daab28512
mozersz <160756785+mozersz@users.noreply.github.com>
2024-04-08 09:54:01 -0300

    Create calculadora2.0.js

---
+++ calculadora2.0.js
@@ -0,0 +1,38 @@
+function addToDisplay(value) {
+    document.getElementById('display').value +=value;
+}
+function clearDisplay() {
+    document.getElementById('display'). value = '';
+}
+function calcular() {
+    var expressao = document.getElementById('display').value;
+    var operandos = expressao.split(/[+\-+\/]/);
+    var operador = expressao.match(/[+\-+\/]/);
+    if (operandos.lenght == 2 || !operador) {
+        document.getElementById('display').value = 'Erro';
+        return;
+    }
+    var num1 = parseFloat(operandos[0]);
+    var num2 = parseFloat(operandos[1]);
+    var resultado;
+    switch (operador[0]) {
+        case '+':
+            resultado =  num1 + num2;
+            break;
+        case '-':
+            resultado =  num1 - num2;
+            break;
+        case '*':
+            resultado =  num1 * num2;
+            break;
+        case '/':
+            if (num2 === 0) {
+                document.getElementById('display').value = 'Erro: divisão por zero';
+                return;
+            }
+            resultado =  num1 / num2;
+            break;
+        default:
+            document.getElementById('diplay').value = resultado;
+        }
+    }
